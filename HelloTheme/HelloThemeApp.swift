94561306c4d0b4c8c8bc81c06ddb7f42c6fd2c38
zeldafox <zeldafoxmail@gmail.com>
2025-08-15 20:32:21 +0800

    Initial Commit

---
+++ HelloTheme/HelloThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloThemeApp.swift
+//  HelloTheme
+//
+//  Created by devlink on 2025/8/15.
+//
+
+import SwiftUI
+
+@main
+struct HelloThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
